eb0a0536ac16d47b5fdadfe9b01ee617467d796b
Francesco Freni <ffreni@student.ethz.ch>
2025-08-04 17:56:56 +0200

    fixed minor mistake

--- fluxnet/dataloader.py
+++ fluxnet/dataloader.py
@@ -25,7 +25,13 @@ def generate_fold_info(df, setting, start, stop=None):
 
 
 def get_fold_df(
-    df, setting, group, target="GPP", remove_missing=False, astorch=False
+    df,
+    setting,
+    group,
+    target="GPP",
+    cv=False,
+    remove_missing=False,
+    astorch=False,
 ):
     # Get the correct data
     if setting == "insite" or setting == "insite-random":
