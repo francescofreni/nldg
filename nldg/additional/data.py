d510fc9edfe1c2a875a3a78acc15c92db6178767
Francesco Freni <ffreni@student.ethz.ch>
2025-10-03 08:02:13 +0200

    modified mu

--- nldg/additional/data.py
+++ nldg/additional/data.py
@@ -44,7 +44,8 @@ class DataContainer:
             self.mu0 = np.zeros(self.d)
         else:
             # self.mu0 = np.array([-0.25, -0.25, 0, 0.25, 0.25])
-            self.mu0 = np.array([1, -1, 0.5, 0, 0])
+            # self.mu0 = np.array([1, -1, 0.5, 0, 0])
+            self.mu0 = np.array([-1, -1, 0, 1, 1])
         X_sample = np.random.randn(1000, self.d) + self.mu0
         for l in range(L):
             # random beta in [-1,1]^d
