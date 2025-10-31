252d4e9e237d510f05b22ca719330db30b9d75fa
Linus Kuehne <kuehne.linus@gmail.com>
2025-10-31 10:42:04 +0100

    use regret

--- experiments/additional/comparison_weights.py
+++ experiments/additional/comparison_weights.py
@@ -3,15 +3,14 @@ import os
 import numpy as np
 from adaXT.random_forest import RandomForest
 from nldg.additional.data_GP import DataContainer
-from nldg.utils import min_reward
+from nldg.utils import min_reward, max_mse, max_regret
 from nldg.additional.gdro import GroupDRO
 from tqdm import tqdm
-from sklearn.model_selection import train_test_split
 from matplotlib.ticker import MaxNLocator
 import matplotlib.pyplot as plt
 
 
-N_SIM = 15
+N_SIM = 25
 N_ESTIMATORS = 100
 MIN_SAMPLES_LEAF = 5
 SEED = 42
@@ -24,8 +23,9 @@ COLORS = {
 }
 
 NUM_COVARIATES = 10
-CHANGE_X_DISTR = True
-risk_label = "nrw"
+CHANGE_X_DISTR = False
+risk = "regret"  # "mse", "reward", "regret"
+risk_label = "reg"  # "mse", "nrw", "reg"
 N_JOBS = 20
 COMMON_CORE_FUNC = False
 
@@ -44,7 +44,6 @@ os.makedirs(OUT_DIR, exist_ok=True)
 def plot_maxrisk_vs_nenvs(
     results: dict[int, dict[str, float]],
     risk_label: str = "mse",
-    loss_type: str = "out-of-sample",
     change_X_distr: bool = False,
 ) -> None:
     methods = []
@@ -106,7 +105,7 @@ def plot_maxrisk_vs_nenvs(
     plt.savefig(
         os.path.join(
             OUT_DIR,
-            f"{loss_type}_comparison_weights_MaxRM_changeXdistr{str(change_X_distr)}_commonCoreFunc{str(COMMON_CORE_FUNC)}.pdf",
+            f"{risk_label}_comparison_weights_MaxRM_changeXdistr{str(change_X_distr)}_commonCoreFunc{str(COMMON_CORE_FUNC)}.pdf",
         ),
         dpi=300,
         bbox_inches="tight",
@@ -122,12 +121,13 @@ if __name__ == "__main__":
             N=2000,
             d=NUM_COVARIATES,
             change_X_distr=CHANGE_X_DISTR,
-            risk="reward",
+            risk=risk,
             target_mode="convex_mixture_P",
+            common_core_func=COMMON_CORE_FUNC,
         )
 
         data.generate_funcs_list(L=L, seed=SEED)
-        max_risks = np.zeros((N_SIM, 5))
+        max_risks = np.zeros((N_SIM, 3))
 
         for sim in tqdm(range(N_SIM), leave=False):
             data.generate_data(seed=sim)
@@ -139,10 +139,29 @@ if __name__ == "__main__":
             Yte = np.concatenate(data.Y_target_potential_list)
             Ete = np.concatenate(data.E_target_potential_list)
 
-            # for weighted random forests with out-of-sample predictions
-            Xtr_t, Xtr_w, Ytr_t, Ytr_w, Etr_t, Etr_w = train_test_split(
-                Xtr, Ytr, Etr, test_size=0.3, random_state=SEED, shuffle=True
-            )
+            if risk == "regret":
+                fitted_erm = np.zeros(len(Etr))
+                fitted_erm_trees = np.zeros((N_ESTIMATORS, len(Etr)))
+                pred_erm = np.zeros(len(Ete))
+                for env in np.unique(Etr):
+                    mask = Etr == env
+                    Xtr_env = Xtr[mask]
+                    Ytr_env = Ytr[mask]
+                    rf_e = RandomForest(
+                        "Regression",
+                        n_estimators=N_ESTIMATORS,
+                        min_samples_leaf=MIN_SAMPLES_LEAF,
+                        seed=SEED,
+                        n_jobs=N_JOBS,
+                    )
+                    rf_e.fit(Xtr_env, Ytr_env)
+                    fitted_erm[mask] = rf_e.predict(Xtr_env)
+                    for i in range(N_ESTIMATORS):
+                        fitted_erm_trees[i, mask] = rf_e.trees[i].predict(
+                            Xtr_env
+                        )
+                    mask_te = Ete == env
+                    pred_erm[mask_te] = rf_e.predict(Xte[mask_te])
 
             # RF ------------------------------------------------------------
             rf = RandomForest(
@@ -159,10 +178,13 @@ if __name__ == "__main__":
 
             # GroupDRO-NN ---------------------------------------------------
             gdro = GroupDRO(
-                data, hidden_dims=[4, 8, 16, 32, 8], seed=SEED, risk="reward"
+                data, hidden_dims=[4, 8, 16, 32, 8], seed=SEED, risk=risk
             )
             gdro.fit(epochs=500)
             pred_gdro = gdro.predict(Xte)
+
+            if risk == "regret":
+                pred_erm_gdro = gdro.predict_per_group(Xte, Ete)
 
             # ---------------------------------------------------------------
 
@@ -170,6 +192,9 @@ if __name__ == "__main__":
             solvers = ["ECOS", "SCS"]
             success = False
             kwargs = {"n_jobs": N_JOBS}
+            if risk == "regret":
+                kwargs["sols_erm"] = fitted_erm
+                kwargs["sols_erm_trees"] = fitted_erm_trees
             for solver in solvers:
                 try:
                     rf.modify_predictions_trees(
@@ -193,85 +218,36 @@ if __name__ == "__main__":
 
             # ---------------------------------------------------------------
 
-            # W-RF-2 --------------------------------------------------------
-
-            # fit trees on a part of the training data
-            rf_t = RandomForest(
-                "Regression",
-                n_estimators=N_ESTIMATORS,
-                min_samples_leaf=MIN_SAMPLES_LEAF,
-                seed=SEED,
-                n_jobs=N_JOBS,
-            )
-            rf_t.fit(Xtr_t, Ytr_t)
-
-            pred_wrf2, weights_wrf2 = rf_t.refine_weights(
-                X_val=Xtr_w,
-                Y_val=Ytr_w,
-                E_val=Etr_w,
-                X=Xte,
-                risk=risk_label,
-            )
-
-            # ---------------------------------------------------------------
-
-            # W-MaxRM-RF-2 --------------------------------------------------
-            solvers = ["ECOS", "SCS"]
-            success = False
-            kwargs = {"n_jobs": N_JOBS}
-            for solver in solvers:
-                try:
-                    rf_t.modify_predictions_trees(
-                        Etr_t,
-                        method="reward",
-                        **kwargs,
-                        solver=solver,
-                    )
-                    success = True
-                    break
-                except Exception as e_try:
-                    pass
-            if not success:
-                rf_t.modify_predictions_trees(
-                    Etr_t,
-                    method="reward",
-                    **kwargs,
-                    opt_method="extragradient",
-                )
-
-            pred_w_maxrmf_2, weights_w_maxrmf_2 = rf_t.refine_weights(
-                X_val=Xtr_w,
-                Y_val=Ytr_w,
-                E_val=Etr_w,
-                X=Xte,
-                risk=risk_label,
-            )
-
-            # ---------------------------------------------------------------
-
             # Evaluate the maximum risk
-            max_risks[sim, 0] = -min_reward(Yte, pred_rf, Ete)
-            max_risks[sim, 1] = -min_reward(Yte, pred_maxrmrf, Ete)
-            max_risks[sim, 2] = -min_reward(Yte, pred_wrf2, Ete)
-            max_risks[sim, 3] = -min_reward(Yte, pred_w_maxrmf_2, Ete)
-            max_risks[sim, 4] = -min_reward(Yte, pred_gdro, Ete)
+
+            if risk == "mse":
+                max_risks[sim, 0] = max_mse(Yte, pred_rf, Ete)
+                max_risks[sim, 1] = max_mse(Yte, pred_maxrmrf, Ete)
+                max_risks[sim, 2] = max_mse(Yte, pred_gdro, Ete)
+            elif risk == "reward":
+                max_risks[sim, 0] = -min_reward(Yte, pred_rf, Ete)
+                max_risks[sim, 1] = -min_reward(Yte, pred_maxrmrf, Ete)
+                max_risks[sim, 2] = -min_reward(Yte, pred_gdro, Ete)
+            else:
+                max_risks[sim, 0] = max_regret(Yte, pred_rf, pred_erm, Ete)
+                max_risks[sim, 1] = max_regret(
+                    Yte, pred_maxrmrf, pred_erm, Ete
+                )
+                max_risks[sim, 2] = max_regret(
+                    Yte, pred_gdro, pred_erm_gdro, Ete
+                )
 
         results[L]["RF"] = max_risks[:, 0].tolist()
         results[L][f"MaxRM-RF({risk_label})"] = max_risks[:, 1].tolist()
-        results[L][f"Weighted RF({risk_label})"] = max_risks[:, 2].tolist()
-        results[L][f"Weighted MaxRM-RF({risk_label})"] = max_risks[
-            :, 3
-        ].tolist()
-        results[L][f"GroupDRO-NN({risk_label})"] = max_risks[:, 4].tolist()
+        results[L][f"GroupDRO-NN({risk_label})"] = max_risks[:, 2].tolist()
 
     np.save(
-        f"results_changeXdistr_{CHANGE_X_DISTR}_commonCoreFunc_{COMMON_CORE_FUNC}.npy",
+        f"({risk_label})_results_changeXdistr_{CHANGE_X_DISTR}_commonCoreFunc_{COMMON_CORE_FUNC}.npy",
         results,
     )
 
     plot_maxrisk_vs_nenvs(
         results,
         risk_label=risk_label,
-        loss_type="out-of-sample",
         change_X_distr=CHANGE_X_DISTR,
     )
