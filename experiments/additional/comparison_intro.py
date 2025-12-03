f8a7d7ff7fd0a9ee8443cc9801fcd409e45fed67
Linus Kuehne <kuehne.linus@gmail.com>
2025-12-03 12:12:32 +0100

    rename new group DRO implementation

--- experiments/additional/comparison_intro.py
+++ experiments/additional/comparison_intro.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from nldg.rf import MaggingRF
 from adaXT.random_forest import RandomForest
 from tqdm import tqdm
-from nldg.additional.gdro_new import GroupDRO
+from nldg.nldg.additional.gdro import GroupDRO
 
 N_ESTIMATORS = 100
 MIN_SAMPLES_LEAF = 15
